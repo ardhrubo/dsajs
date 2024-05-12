33588756b44e7dbd8a358652631b1fd5f648ec2a
ardhrubo <ardhrubo3038@gmail.com>
2024-05-12 19:49:47 +0600

    fibonacci series

--- chapter_one/index.js
+++ chapter_one/index.js
@@ -1,1 +1,21 @@
-console.log('Hello, World!')
+// Given a number n , find the n elements of the Fibonacci sequence.
+// Determine the big O of your solution.
+
+
+function fibonacci(n){
+    const fib = [0,1];
+    for(let i=2;i<n;i++){
+
+        fib[i] = fib[i-1] + fib[i-2]
+
+    }
+
+    return fib
+}
+
+
+console.log(fibonacci(3))
+
+
+
+// Linear time complexity O(n) = Big O(n)
