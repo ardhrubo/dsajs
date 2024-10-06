d03282415cffd95be665b305c277b87e0354b779
ardhrubo <ardhrubo3038@gmail.com>
2024-10-06 17:45:08 +0600

    queue

--- dataStructure/chapter_two_custom_ds/stack/stack.js
+++ dataStructure/chapter_two_custom_ds/stack/stack.js
@@ -19,14 +19,93 @@ The stack data structure has two main operations:
 
 */
 
+class Stack {
+    constructor(){
+        this.items = []
+        this.count = 0
+    }
+    push(element){
+        this.items[this.count] = element
+        this.count+=1
+        return this.count - 1
+    }
+
+    // Return and remove the top element in the stack
+    // Return undefined if the stack is empty
+
+    pop(){
+        if(this.count ==0) return undefined
+        let deleteItem = this.items[this.count -1]
+        this.count -=1
+        console.log(`${deleteItem} removed`)
+        return deleteItem
+    }
+
+    // Check the top element in the stack
+    peek(){
+
+        return this.items[this.count-1]
+    }
+
+    // Check if the stack is empty
+
+    isEmpty(){
+        return this.count == 0
+    }
+
+    // Check the size of the stack
+
+    size(){
+        return this.count
+    }
+
+    // Print the elements in the stack
+    print(){
+        let str = ''
+        for(let i=0;i<this.count;i++){
+            str+=this.items[i] + ' '
+            
+        }
+        return str
+    }
+
+    // Clear the stack
+    clear(){
+        this.items = []
+        this.count = 0
+        return this.items
+    }
 
 
+}
+
+const stack = new Stack()
+
+stack.push(100)
+stack.push(200)
+stack.push(300)
+
+// console.log(stack.peek())
+
+stack.push(400)
+console.log(stack)
+
+stack.pop()
+// console.log(stack.clear())
+
+console.log(stack.isEmpty())
+
+console.log(stack.print())
+
+stack.pop()
+stack.pop()
+// console.log(stack.print())
 
 
+// stack.pop()
 
 
-
-
-
+// console.log(stack)
+// console.log(stack.clear())
 
 
